ff45030f9f84adc345eecf5a853f464caf8446b0
Phong Nguyen <phongnd.dev@gmail.com>
2021-12-19 02:13:46 +0700

    Test controller

---
+++ src/main/java/com/aptech/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.aptech.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	
+	@PostMapping("/register")
+	public String register() {
+		return "Ok";
+	}
+	
+}
