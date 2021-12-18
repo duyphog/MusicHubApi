814ab9e1c048f0f34fa3da9f67576cf9217ac84a
duyphong-git <phongnddev@outlook.com>
2021-12-18 22:26:58 +0700

    Initialize project

---
+++ src/main/java/com/aptech/MusicHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.aptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicHubApiApplication.class, args);
+	}
+
+}
