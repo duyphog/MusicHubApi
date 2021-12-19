821e31ba8509d05b0dec382709d2d3ce15819d9d
Phong Nguyen <phongnd.dev@gmail.com>
2021-12-19 13:19:37 +0700

    app handling exception

---
+++ src/main/java/com/aptech/handle/exception/AppExceptionHandling.java
@@ -0,0 +1,39 @@
+package com.aptech.handle.exception;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.aptech.domain.HttpResponse;
+import com.aptech.domain.HttpResponseError;
+
+@RestControllerAdvice
+public class AppExceptionHandling {
+
+	private final Logger logger = LogManager.getLogger(AppExceptionHandling.class);
+
+	// Email
+	@ExceptionHandler(EmailExistException.class)
+	public ResponseEntity<HttpResponse> emailExistException(EmailExistException exception) {
+		return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+	}
+
+	@ExceptionHandler(EmailNotFoundException.class)
+	public ResponseEntity<HttpResponse> emailNotFoundException(EmailNotFoundException exception) {
+		return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+	}
+
+	// UserName
+	@ExceptionHandler(UsernameExistException.class)
+	public ResponseEntity<HttpResponse> usernameExistException(UsernameExistException exception) {
+		return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+	}
+
+	private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
+		HttpResponse httpResponse = new HttpResponseError(httpStatus, httpStatus.getReasonPhrase(), message);
+		return new ResponseEntity<>(httpResponse, httpStatus);
+	}
+}
