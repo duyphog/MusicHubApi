5773b581788e1ffdd91653176caa7170b4615318
Phong Nguyen <phongnd.dev@gmail.com>
2021-12-19 13:21:01 +0700

    Add base user controller

--- src/main/java/com/aptech/controller/UserController.java
+++ src/main/java/com/aptech/controller/UserController.java
@@ -1,17 +1,55 @@
 package com.aptech.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aptech.domain.HttpResponse;
+import com.aptech.domain.HttpResponseSuccess;
+import com.aptech.dto.UserDto;
+import com.aptech.dto.UserRegister;
+import com.aptech.handle.exception.EmailExistException;
+import com.aptech.handle.exception.UsernameExistException;
+import com.aptech.service.ipml.AppUserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
-	
+
+	@Autowired
+	private AppUserService appUserService;
+
 	@PostMapping("/register")
-	public String register() {
-		return "Ok";
+	public ResponseEntity<HttpResponse> register(@Valid @RequestBody UserRegister userRegister)
+			throws EmailExistException, UsernameExistException {
+		UserDto user = appUserService.register(userRegister);
+
+		return ResponseEntity.ok(new HttpResponseSuccess<UserDto>(user));
 	}
-	
+
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+	}
+
 }
