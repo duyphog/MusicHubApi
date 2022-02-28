5e9ac6e912b5001682d6ab55fe690d5b002ab497
Phong Nguyen <phongnd.dev@gmail.com>
2022-02-28 22:03:35 +0700

    remove config file

--- src/main/java/com/aptech/constant/FileConstant.java
+++ src/main/java/com/aptech/constant/FileConstant.java
@@ -1,20 +1,5 @@
 package com.aptech.constant;
 
-public class FileConstant {
-    private static final String IMAGE_FOLDER = System.getProperty("user.home") + "/Documents/MyDoc/Aptech/Aptech/springboot/e-project/static-file/images/";
-    public static final String TRACK_FOLDER = System.getProperty("user.home") + "/Documents/MyDoc/Aptech/Aptech/springboot/e-project/static-file/songs/";
-    
-    public static String IMAGE_FOLDER_DEFAULT = IMAGE_FOLDER;
-    public static final String USER_IMAGE_FOLDER = IMAGE_FOLDER + "users/";
-    public static final String ALBUM_IMGAGE_FOLDER = IMAGE_FOLDER + "albums/";
-    public static final String TRACK_IMGAGE_FOLDER = IMAGE_FOLDER + "songs/";
-    
+public class FileConstant {    
     public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
-    public static final String DEFAULT_USER_MOCK_IMAGE_PATH = "/user/image/profile/";
-    
-    // Path
-    public static final String USER_IMAGE_PATH = "/myfile/images/users/";
-    public static final String ALBUM_IMAGE_PATH = "/myfile/images/albums/";
-    public static final String TRACK_IMAGE_PATH = "/myfile/images/songs/";
-    public static final String TRACK_PATH = "/myfile/songs/";
 }
